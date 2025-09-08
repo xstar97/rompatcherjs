e0f21d657c3c4f5e21017a210acb47bee86bfb42
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2025-09-08 08:14:20 -0400

    update

--- scripts/update-readme.sh
+++ scripts/update-readme.sh
@@ -4,15 +4,18 @@ set -e
 VERSION="$1"
 TEMPLATE="README.template.md"
 OUTPUT="README.md"
+VERSION_FILE="VERSION"
 
-# If no version is provided, fetch the latest tag from GitHub
+# If no version provided, fetch latest upstream tag
 if [ -z "$VERSION" ]; then
-  echo "No VERSION provided, fetching latest tag from GitHub..."
-  VERSION=$(curl -s https://api.github.com/repos/xstar97/rompatcherjs/tags | jq -r '.[0].name')
-  echo "Using latest tag: $VERSION"
+  echo "No VERSION provided, fetching latest upstream tag..."
+  VERSION=$(curl -s https://api.github.com/repos/marcrobledo/RomPatcher.js/releases/latest | jq -r '.tag_name')
 fi
 
 echo "Updating README with version $VERSION..."
 
-# Use | as sed delimiter to avoid problems with / in VERSION
+# Update README.md from template
 sed "s|{{VERSION}}|$VERSION|g" "$TEMPLATE" > "$OUTPUT"
+
+# Update VERSION file
+echo "$VERSION" > "$VERSION_FILE"
