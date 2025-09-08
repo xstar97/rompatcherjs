055c2d8c7c33c2f3681586cf3d51afaee5702f65
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2025-09-08 07:58:47 -0400

    update

--- scripts/update-readme.sh
+++ scripts/update-readme.sh
@@ -1,15 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 VERSION="$1"
 TEMPLATE="README.template.md"
 OUTPUT="README.md"
 
+# If no version is provided, fetch the latest tag from GitHub
 if [ -z "$VERSION" ]; then
-  echo "Usage: $0 <version>"
-  exit 1
+  echo "No VERSION provided, fetching latest tag from GitHub..."
+  VERSION=$(curl -s https://api.github.com/repos/xstar97/rompatcherjs/tags | jq -r '.[0].name')
+  echo "Using latest tag: $VERSION"
 fi
 
 echo "Updating README with version $VERSION..."
 
-sed "s/{{VERSION}}/$VERSION/g" "$TEMPLATE" > "$OUTPUT"
+# Use | as sed delimiter to avoid problems with / in VERSION
+sed "s|{{VERSION}}|$VERSION|g" "$TEMPLATE" > "$OUTPUT"
